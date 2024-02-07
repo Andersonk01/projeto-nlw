4f77e617eaceeb7ed630bf3d9624a335f3df5931
Andersonk01 <andersonreuak@gmail.com>
2024-02-07 20:13:19 -0300

    "create-getPolls-and-update"

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,10 @@
 import fastify from 'fastify';
 import { createPoll } from '../routes/create-poll';
+import { getPoll } from '../routes/get-poll';
 const app = fastify();
 
 app.register(createPoll);
+app.register(getPoll);
 
 app.listen({ port: 3000 }).then(() => {
   console.log('Server is running on port 3000');
--- src/routes/create-poll.ts
+++ src/routes/create-poll.ts
@@ -6,9 +6,10 @@ export async function createPoll(app: FastifyInstance) {
   app.post('/polls', async (request, reply) => {
     const createPollBody = z.object({
       title: z.string(),
+      options: z.array(z.string()),
     });
 
-    const { title } = createPollBody.parse(request.body); // Validate the request body
+    const { title, options } = createPollBody.parse(request.body); // Validate the request body
 
     const poll = await prisma.poll.create({
       data: {
@@ -16,25 +17,18 @@ export async function createPoll(app: FastifyInstance) {
       },
     });
 
+    // não é possível utilizar o createMany com o SQLite, por isso o Promise.all
+    await Promise.all(
+      options.map((option) => {
+        return prisma.pollOption.create({
+          data: {
+            title: option,
+            pollId: poll.id,
+          },
+        });
+      })
+    );
+
     return reply.status(201).send({ pollId: poll.id });
   });
 }
-
-// Primeiro, criar a poll, sem options.
-// const poll = await prisma.poll.create({
-//   data: {
-//     title,
-//   },
-// });
-
-// Em seguida, utilizar um Promise.all para criar todas as options:
-// await Promise.all(
-//   options.map((option) => {
-//     return prisma.pollOption.create({
-//       data: {
-//         title: option,
-//         pollId: poll.id,
-//       },
-//     });
-//   })
-// );
